b3a51caad7ddd02b458a5dc3ada0e37ea3f52f5a
DevDelvo <kevinbdelvo@gmail.com>
2018-07-28 13:19:21 -0400

    added bubbleSort solution

---
+++ Easy/bubbleSort.js
@@ -0,0 +1,30 @@
+//Bubble Sort
+
+// Implement bubble sort algorithm in a function
+
+//Sample input: [8,5,2,9,5,6,3]
+//Sample input: [2,3,5,5,6,8,9]
+
+function bubbleSort(arr) {
+    let isSorted = false;
+
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swapper(i, i+1, arr);
+                isSorted = false;
+            }
+        }
+    }
+    console.log(arr);
+    return arr;
+}
+
+function swapper(i, j, arr) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+bubbleSort([8,5,2,9,5,6,3])
