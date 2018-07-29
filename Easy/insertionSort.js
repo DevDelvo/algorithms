9e4f4343ac24dd50875ab613ad7ab64e3ac09887
DevDelvo <kevinbdelvo@gmail.com>
2018-07-29 10:57:08 -0400

    added Big O notation to insertionSort.js

--- Easy/insertionSort.js
+++ Easy/insertionSort.js
@@ -6,21 +6,25 @@
 //Somple input: [8,5,2,9,5,6,3]
 //Sample output: [2,3,5,5,6,8,9]
 
+
+// Best O(n) time | O(1) space
+// Average: O(n^2) time | O(1) space
+// Worst: O(n^2) time | O(1) space
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let j = i;
         while (j > 0 && arr[j] < arr[j - 1]) {
             swap(j, j - 1, arr);
-            j-=1;
+            j--;
         }
     }
     console.log(arr)
     return arr;
 }
 
-function swap(i, j, arr) {
-    const temp = arr[j];
-    arr[j] = arr[i];
+function swap(i, prev, arr) {
+    const temp = arr[prev];
+    arr[prev] = arr[i];
     arr[i] = temp;
 }
 
