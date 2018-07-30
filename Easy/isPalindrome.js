f78b23f1ad122536ba8656cf734ae0cacd655062
DevDelvo <kevinbdelvo@gmail.com>
2018-07-30 12:58:33 -0400

    added other solutions and Big O for palindrome check

--- Easy/isPalindrome.js
+++ Easy/isPalindrome.js
@@ -6,6 +6,40 @@
 //Sample input: "racecar"
 //Sample output: True(it is a palindrome)
 
+//SOLUTION 1
+// O(n^2) time | O(n) space
+function isPalindrome1(str) {
+    let reversed = "";
+    for (let i = str.length -1; i >= 0; i--) {
+        reversed += str[i]; //when you concatenate the string you are creating a new string each time making O(n^2)
+    }
+    return str === reversed;
+}
+// Solution 2
+// O(n) time | O(n space)
+function isPalindrome2(str) {
+    const reversed = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed.push(str[i]);
+    }
+    return str === reversed.join("");
+}
+
+// Solution 3
+// O(n) time | O(n) space
+function isPalindrome3(str, i = 0) {
+    const j = str.length - 1 - i;
+    if (i >= j) {
+        console.log('recursion', true)
+        return true;
+    } else {
+        return str[i] === str[j] && isPalindrome3(str, i + 1);
+    }
+}
+
+isPalindrome3('racecar')
+
+
 // O(n) time | O(1) space
 function isPalindrome(str) {
     let left = 0;
