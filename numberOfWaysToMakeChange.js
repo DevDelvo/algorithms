85466dea22e5857eb27c461c9a4be736a188ea1a
DevDelvo <kevinbdelvo@gmail.com>
2018-08-18 19:18:53 -0400

    added more explanation to makeChange.js

--- numberOfWaysToMakeChange.js
+++ numberOfWaysToMakeChange.js
@@ -1,13 +1,14 @@
 
-// O(nd) time | O(n) space 
+// O(nd) time | O(n) space
+// where n = target and d = number of coins
 function makeChange(coins, target) {
     let combinations = (new Array(target + 1)).fill(0);
     combinations[0] = 1; //base case. If the target is 0. There is only 1 way to give 0 as change which is by not giving change
     for (let coin of coins) {
-        for (let amount = 1; amount < target + 1; amount++) {
+        for (let amount = 1; amount <= target; amount++) { //will iterate the entire combinations array
             if (coin <= amount) {
                 combinations[amount] += combinations[amount - coin];
-                console.log('coin:', coin,"| combos:", combinations);
+                console.log("amount is", amount, '. coin:', coin,"| combos:", combinations);
             }
         }
     }
