4360d5af53f98725423e1af7b10742d96400de73
DevDelvo <kevinbdelvo@gmail.com>
2018-11-03 13:14:37 -0400

    added loopDetection challenge description

--- dataStructures/Linked List/loopDetection.js
+++ dataStructures/Linked List/loopDetection.js
@@ -1,5 +1,15 @@
 const LinkedList = require('./linkedList');
 
+
+// A linked list is said to contain a cycle if any node is visited more than once while traversing the list.
+
+// Complete the function provided for you in your editor.
+// It has one parameter: a pointer to a Node object named `head` that points to the head of a linked list.
+// Your function must return a boolean denoting whether or not there is a cycle in the list.
+// If there is a cycle, return true; otherwise, return false.
+
+// Note: If the list is empty, `head` will be null.
+
 function detectLoop (head) {
     let firstPointer = head;
     let secondPointer = head;
