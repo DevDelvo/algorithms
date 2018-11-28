0b3e8916433638ab52191f2a565fec75dd335847
DevDelvo <kevinbdelvo@gmail.com>
2018-11-28 00:34:43 -0500

    fix(edge case for maxSubsetNoAdjacent solution. set value for cloned array at index 0)

--- dynamicProgramming/maxSubsetSumNoAdjacent.js
+++ dynamicProgramming/maxSubsetSumNoAdjacent.js
@@ -12,9 +12,10 @@ function maxSubsetSumNoAdjacent(arr) {
     if (arr.length === 1) return arr[0];
 
     const maxSums = arr.slice();
-    maxSums[1] = Math.max(arr[0], arr[1]);
+    maxSums[0] = Math.max(0, maxSums[0])
+    maxSums[1] = Math.max(maxSums[0], maxSums[1]);
     for (let i = 2; i < arr.length; i++) {
-        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + arr[i]);
+        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + maxSums[i]);
     }
     return maxSums[maxSums.length-1];
 }
@@ -34,15 +35,15 @@ function maxSubsetSumNoAdjacent2(arr) {
     return first;
 }
 
-// const test1 = maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]); // 330
-// const test2 = maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]); //33
-// const test3 = maxSubsetSumNoAdjacent([1, 15, 3]); // 15
-// const test4 = maxSubsetSumNoAdjacent([]); // 0
+const test1 = maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]); // 330
+const test2 = maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]); //33
+const test3 = maxSubsetSumNoAdjacent([1, 15, 3]); // 15
+const test4 = maxSubsetSumNoAdjacent([]); // 0
 
-const test1 = maxSubsetSumNoAdjacent2([75, 105, 120, 75, 90, 135]); // 330
-const test2 = maxSubsetSumNoAdjacent2([7, 10, 12, 7, 9, 14]); //33
-const test3 = maxSubsetSumNoAdjacent2([1, 15, 3]); // 15
-const test4 = maxSubsetSumNoAdjacent2([]); // 0
+// const test1 = maxSubsetSumNoAdjacent2([75, 105, 120, 75, 90, 135]); // 330
+// const test2 = maxSubsetSumNoAdjacent2([7, 10, 12, 7, 9, 14]); //33
+// const test3 = maxSubsetSumNoAdjacent2([1, 15, 3]); // 15
+// const test4 = maxSubsetSumNoAdjacent2([]); // 0
 
 console.log(test1);
 console.log(test2);
