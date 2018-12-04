663e43a8950e53315e5d6aeaedaab5102f41eb9a
DevDelvo <kevinbdelvo@gmail.com>
2018-12-04 00:45:43 -0500

    added greedy algorithms folder with deleteColumnsToMakeSorted.js added robotReturnToOrigin and uniqueEmailAddresses solutions from leetcode

--- leetcode/uniqueEmailAddresses.js
+++ leetcode/uniqueEmailAddresses.js
@@ -1,39 +1,35 @@
 // Every email consists of a local name and a domain name, separated by the @ sign.
-
 // For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
-
 // Besides lowercase letters, these emails may contain '.'s or '+'s.
-
 // If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
-
 // If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
-
 // It is possible to use both of these rules at the same time.
-
 // Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails?
 
 // Example 1:
-
 // Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
 // Output: 2
 // Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
 
-// O(n) space | O(n) time
-const uniqueEmailAddress = (emails) => {
-    const map = new Map();
+// Note:
+// 1 <= emails[i].length <= 100
+// 1 <= emails.length <= 100
+// Each emails[i] contains exactly one '@' character.
+
+const uniqueEmailAddressses = (emails) => {
+    const uniqueEmails = new Map();
     for (const email of emails) {
-        let local = email.slice(0, email.indexOf('@'));
-        let rest = email.slice(email.indexOf('@'));
-        if (local.includes("+")) {
-            local = local.slice(0, local.indexOf('+')); // remove all the test after '+'.
+        const localName = email.slice(0, email.indexOf("@"));
+        const domainName = email.slice(indexOf("@"));
+        if (localName.includes("+")) {
+            localName = localName.slice(0,localName.indexOf("+"));
         }
-        local = local.replace(/\./g, ""); // replace all the '.' with ''.
-        map.set(local + rest);
+        localName.replace(/\./g, "");
+        uniqueEmails.set(localName + domainName, true);
     }
-    console.log(map);
-    return map.size;
+    console.log(uniqueEmails.has('testemail@leetcode.com'))
+    return uniqueEmails.size;
 }
 
-const test1 = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"];
-
-console.log(uniqueEmailAddress(test1));
+const test1 = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]; // 2
+console.log(uniqueEmailAddressses(test1))
