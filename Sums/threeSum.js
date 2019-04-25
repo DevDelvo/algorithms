a929099ef5a53507b41fcc08544e1cbd344baa53
devdelvo <kevinbdelvo@gmail.com>
2019-04-24 20:28:41 -0400

    added O(n^2) 3sum solution from stackoverFlow

---
+++ Sums/threeSum.js
@@ -0,0 +1,50 @@
+// Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+// Find all unique triplets in the array which gives the sum of zero.
+
+// Note:
+// The solution set must not contain duplicate triplets.
+
+// Example:
+// Given array nums = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
+
+const threeSum = (arr) => {
+    let solutions = [];
+    var target = 0;
+    arr.sort((a, b) => {
+        return a - b;
+    });
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (i === 0 || (i > 0 && arr[i] !== arr[i-1])) {
+            let low = i + 1;
+            let high = arr.length - 1;
+            let sum = - arr[i];
+            while ( low < high) {
+                if (arr[low] + arr[high] === sum) {
+                    solutions.push([arr[i], arr[low], arr[high]]);
+                    while (low < high && arr[low] === arr[low + 1]) low++;
+                    while (low < high && arr[high] === arr[high - 1]) high--;
+                    low++;
+                    high--;
+                } else if (arr[low] + arr[high] > sum) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+        }
+    }
+    return solutions;
+}
+
+const test1 = [-1, 0, 1, 2, -1, -4];
+
+
+console.log(threeSum(test1)); // expects [[-1, 0, 1], [-1, -1, 2]];
+// console.log(threeSum(test2));
+// console.log(threeSum(test3));
