bd6b0dbc508fc9a450eb23265581020d5fd25eb1
DevDelvo <kevinbdelvo@gmail.com>
2019-09-20 21:17:13 -0400

    added optimized sliding window solution for longestSubstringWithoutRepeatingCharacters.js

--- leetcode/dataStructures/longestSubstringWithoutRepeatingCharacters.js
+++ leetcode/dataStructures/longestSubstringWithoutRepeatingCharacters.js
@@ -31,8 +31,7 @@
 // To check if one string has duplicate characters, we can use a set. We iterate through all the characters in the string and put them into the set one by one.
 // Before putting one character, we check if the set already contains it. If so, we return false. After the loop, we return true.
 
-// Big O
-// O(n^3) Time | O(min(n,m)) Space
+
 function lengthOfLongestSubString(str) {
     let longest = 0;
 
@@ -56,6 +55,8 @@ function isAllUniqueHelper(str, start, end) {
     }
     return true;
 }
+// Big O
+// O(n^3) Time | O(min(n,m)) Space
 
 console.log('Brute force => ', lengthOfLongestSubString("abcabcbb")); // 3
 console.log('Brute force => ', lengthOfLongestSubString("bbbbb")); // 1
@@ -80,12 +81,42 @@ console.log('Brute force => ', lengthOfLongestSubString("pwwkew")); // 3
 // If it is not in the HashSet, we slide j further.
 // Doing so until s[j] is already in the HashSet.
 // At this point, we found the maximum size of substrings without duplicate characters start with index ii. If we do this for all ii, we get our answer.
+
+function lengthOfLongestSubString2(str) {
+    const hashMap = new Map(); // will have character to integer association.
+    let longest = 0;
+    let i = 0, j = 0;
+    while (i < str.length && j < str.length) {
+        // try to extend range of [i, j)
+        if (!hashMap.has(str.charAt(i))) {
+            hashMap.set(str.charAt(i++))
+            longest = Math.max(longest, i - j);
+        } else {
+            hashMap.delete(str.charAt(j++));
+        }
+    }
+    console.log(hashMap.entries())
+    return longest;
+}
 // Big O
 // Time complexity : O(2n) = O(n). In the worst case each character will be visited twice by i and j.
 // Space complexity : O(min(m, n)). Same as the previous approach.
 // We need O(k) space for the sliding window, where k is the size of the Set.
 // The size of the Set is upper bounded by the size of the string n and the size of the charset/alphabet m.
-function lengthOfLongestSubString2(str) {
+
+console.log('Sliding Window => ', lengthOfLongestSubString2("abcabcbb")); // 3
+console.log('Sliding Window => ', lengthOfLongestSubString2("bbbbb")); // 1
+console.log('Sliding Window => ', lengthOfLongestSubString2("pwwkew")); // 3
+
+// Solution 3: Sliding Window Optimized
+// The above solution requires at most 2n steps. In fact, it could be optimized to require only n steps.
+// Instead of using a set to tell if a character exists or not, we could define a mapping of the characters to its index.
+// Then we can skip the characters immediately when we found a repeated character.
+
+// The reason is that if s[j] have a duplicate in the range [i, j) with index j',
+// we don't need to increase ii little by little.
+// We can skip all the elements in the range [i, j']and let i to be j' + 1 directly.
+function lengthOfLongestSubString3(str) {
     const hashMap = new Map(); // will have character to integer association.
     let longest = 0;
     for (let i = 0, j = 0; i < str.length; i++) { // i and j for our window.
@@ -97,7 +128,10 @@ function lengthOfLongestSubString2(str) {
     }
     return longest;
 }
+// Complexity Analysis
+// Time complexity : O(n) Index j will iterate n times.
+// Space complexity (HashMap) : O(min(m, n)) Same as the previous approach.
 
-console.log('Sliding Window => ', lengthOfLongestSubString2("abcabcbb")); // 3
-console.log('Sliding Window => ', lengthOfLongestSubString2("bbbbb")); // 1
-console.log('Sliding Window => ', lengthOfLongestSubString2("pwwkew")); // 3
+console.log('Sliding Window Optimized => ', lengthOfLongestSubString3("abcabcbb")); // 3
+console.log('Sliding Window Optimized => ', lengthOfLongestSubString3("bbbbb")); // 1
+console.log('Sliding Window Optimized => ', lengthOfLongestSubString3("pwwkew")); // 3
