3e8e5d584fa46269c2ffc90cfb34136edf3c4334
devdelvo <kevinbdelvo@gmail.com>
2019-10-18 17:33:27 -0400

    added more testcases

--- daily-challenge/rangeExtraction-#093.js
+++ daily-challenge/rangeExtraction-#093.js
@@ -15,24 +15,30 @@ function rangeExtraction(arr) {
     if (!arr.length) return 0;
     if (arr.length === 1) return arr[0];
     let res = '';
-    let range = [];
+    let range = [arr[0]];
     let pointer1 = 0;
     let pointer2 = 1;
     while (pointer1 < arr.length) {
-        if (pointer1 !== pointer2 && arr[pointer1] + 1 !== arr[pointer2] && range.length === 0) {
-            // console.log('arr[pointer2 -1] => ', arr[pointer2 - 1])
+        if (pointer1 !== pointer2 && arr[pointer1] + 1 !== arr[pointer2] && range.length === 1) {
+            console.log('arr[pointer1] => ', arr[pointer1])
             res += arr[pointer1].toString() + ',';
             pointer1++;
-        } else if (arr[pointer2 + 1] === arr[pointer2] + 1) { // when the next number after pointer2 is +1, put them into range array
-            // console.log('push new range')
-            range.push(arr[pointer1]);
+        } else if (arr[pointer2 + 1] === arr[pointer2] + 1 && range.length === 0) { // when the next number after pointer2 is +1, put them into range array
+            console.log('push new range')
+            range.push(arr[pointer2]);
+            console.log('new range => ', range)
+            // pointer2++;
+        } else if (arr[pointer2 + 1] === arr[pointer2] + 1 && range.length) {
+            console.log('push new range, range length > 0')
+            range.push(arr[pointer2]);
+            if (range[1] && range[1] !== range[0] + 1) range.shift();
             pointer2++;
         } else if (arr[pointer2 + 1] !== arr[pointer2] + 1) {// when the next number after pointer2 is NOT +1, add them to the res and reset array
+            console.log('pointer1 => ', pointer1 , 'pointer2 => ', pointer2)
             range.push(arr[pointer2]);
-            // console.log('final range => ', range)
-            range.length >= 3 ? res += range[0].toString() + '-' + range[range.length - 1].toString() + ',' : res += range[0] + ',' + range[1] + ',';
-            // res += range[0].toString() + '-' + range[range.length - 1].toString() + ','; // add to res
-            // console.log('res => ', res)
+            console.log('final range => ', range)
+            range.length >= 3 ? res += range[0].toString() + '-' + range[range.length - 1].toString() + ',' : range.length === 2 ? res += range[0] + ',' + range[1] + ',' : res += range[0] + ",";
+            console.log('res after final range => ', res)
             range = []; // reset array
             pointer1 = pointer2; // jumpstart pointer 1 to pointer 2
             pointer1++; //skip pointer1 to avoid duplicate
@@ -43,5 +49,6 @@ function rangeExtraction(arr) {
 }
 
 console.log(rangeExtraction([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])); //-6,-3-1,3-5,7-11,14,15,17-20
-console.log(rangeExtraction([1,2,3,4,5,6,7,8,9,10])); // 1,2,3,4,5,6,7,8,9,10
-console.log(rangeExtraction([1])) // 1
+// console.log(rangeExtraction([1,2,3,4,5,6,7,8,9,10])); // 1,2,3,4,5,6,7,8,9,10
+// console.log(rangeExtraction([1])) // 1
+console.log(rangeExtraction([-3,-2,-1,2,10,15,16,18,19,20])) //  -3--1,2,10,15,16,18-20
