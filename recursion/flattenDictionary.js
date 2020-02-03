d0a8cde1e0f0572198e651352982c9deef72aebc
DevDelvo <kevinbdelvo@gmail.com>
2020-02-03 12:54:33 -0500

    added join solution from hatchways prep

--- recursion/flattenDictionary.js
+++ recursion/flattenDictionary.js
@@ -1,18 +1,18 @@
-// The question is given an object / dictionary where the keys are strings, 
-// and the values are either strings or another object / dictionary, can you 
+// The question is given an object / dictionary where the keys are strings,
+// and the values are either strings or another object / dictionary, can you
 // return a new object / dictionary with the keys flattened.
 
 // For example, { a: "b", c: { d: "e", "f": {g: "e"} } }, would become { a: "b", "c.d": "e", c.f: "g" }.
-// Notice that this function will remove any of the nested objects / dictionaries, 
+// Notice that this function will remove any of the nested objects / dictionaries,
 // and will combine the keys in the nested objects / dictionaries to be joined by a period.
 
 function flatten(dict) {
   // step through each property
   // if string, continue?
-  // if object, append to the parent and 
+  // if object, append to the parent and
   const flattenedDictionary = {};
-  
-  for (const key in dict){
+
+  for (const key in dict) {
     const value = dict[key];
     const parent = key;
     if (typeof value === 'object') {
@@ -38,4 +38,5 @@ function flattenHelper(parent, dict, flattenedDict) {
 
 
 console.log(flatten({ a: "b", c: { d: "e" } }));
-console.log(flatten({ a: "b", c: { d: "e", f: {e:"g"}} }))
+console.log(flatten({ a: "b", c: { d: "e", f: { e: "g" } } }))
+console.log(flatten({ a: "b", c: { d: "e", f: 'g' } }))
